d2ea8f1c27e540e9396aa211cc4b6deefe574c46
higerra <higerra@126.com>
2015-11-21 17:37:24 -0600

    Hang

--- studio/reactor/client/client.cpp
+++ studio/reactor/client/client.cpp
@@ -3,13 +3,11 @@
 //
 
 #include "client.h"
-#include <ace/INET_Addr.h>
-#include <ace/SOCK_Connector.h>
 
 using namespace std;
 
 Client::Client(int argc, char **argv, const unsigned short port_):
-        port(port_){
+    port(port_), counter(0){
     messages.resize(argc - 1);
     for(int i=0; i<messages.size(); i++){
         messages[i] = string(argv[i+1]);
@@ -22,21 +20,30 @@ int Client::handle_timeout(const ACE_Time_Value &value, const void *pVoid) {
     ACE_SOCK_Stream stream;
     if(connector.connect(stream,addr) < 0){
         cerr << "Can not connect to server!" <<endl;
-        return 0;
+        return -1;
     }
 
     char space = ' ';
+    char numbuffer[10];
+    
+    sprintf(numbuffer, "%d", counter++);
+    if (stream.send(numbuffer, strlen(numbuffer)) < 0) {
+	cerr << "Can not send message" << endl;
+	return -1;
+    }
     for(int i=0; i<messages.size(); i++) {
         if (stream.send(messages[i].c_str(), messages[i].size()) < 0) {
             cerr << "Can not send message" << endl;
-            return 0;
+            return -1;
         }
         if (stream.send(&space,1) < 0) {
             cerr << "Can not send message" << endl;
-            return 0;
+            return -1;
         }
     }
+    printf("data sent\n");
     stream.close();
-    return 1;
+    cout << "stream closed"<<endl;
+    return 0;
     //return ACE_Event_Handler::handle_timeout(current_time, act);
 }
