8360c168c057ec90fd466ec150fad3ba232b3462
higerra <higerra@126.com>
2015-11-24 16:36:35 -0600

    hang

---
+++ lab3/director/director_ace.cpp
@@ -0,0 +1,1 @@
+#include "director_ace.h"
---
+++ lab3/director/director_ace.h
@@ -0,0 +1,10 @@
+#ifndef DIRECTOR_H
+#define DIRECTOR_H
+#include <ace/ACE.h>
+#include <ace/Acceptor.h>
+#include <ace/Connector.h>
+#include <ace/Svc_Handler.h>
+class DirectorInputHandler: public ACE_Sve_Handler{
+    
+};
+#endif
