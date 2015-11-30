3366d3954621db0d13d7fe735c4491aca183f9fc
higerra <higerra@126.com>
2015-11-30 14:32:21 -0600

    hang

--- lab3/signal_handler.cpp
+++ lab3/signal_handler.cpp
@@ -1,4 +1,4 @@
 #include "signal_handler.h"
 
-std::atomic<bool> SignalHandler::interrupt_flag = false;
-std::atomic<bool> SignalHandler::quit_flag = false;
+std::atomic<bool> SignalHandler::interrupt_flag(false);
+std::atomic<bool> SignalHandler::quit_flag (false);
